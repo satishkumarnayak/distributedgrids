9f327faf5d19ba7b3182f6b266a782f6cb2fed5f
Satish Nayak <saishkumarNayak@gmail.com>
2019-07-29 16:49:11 +0530

    Initial setup

---
+++ src/main/java/com/instanceofcake/distributedgrids/DistributedGridsApplication.java
@@ -0,0 +1,13 @@
+package com.instanceofcake.distributedgrids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedGridsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedGridsApplication.class, args);
+	}
+
+}
